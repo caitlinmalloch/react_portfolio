bb4e47a5d17148bcc0a6035056b32df2f0de0fa9
Caitlin Malloch <caitlin.malloch@gmail.com>
2024-01-05 22:20:10 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return <div>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
